18f9a40afdce331bc823533e912c8e7b99318576
browndove <bluebird23szn@gmail.com>
2024-04-04 03:27:28 -0700

    trial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Particles from "@tsparticles/react";
+import ParticlesComponent from "./components/Particles";
+
+function App() {
+  return (
+    <div className="App">
+     <ParticlesComponent id="particles" />
+    </div>
+  );
+}
+
+export default App;
